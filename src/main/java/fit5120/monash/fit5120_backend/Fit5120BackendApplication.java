9340df6729503b6678fe01cb84cc9c3d181120d5
Xiaojun Liang <xlia0062@student.monash.edu>
2025-04-03 17:35:30 +1100

    Initial Springboot project setup

---
+++ src/main/java/fit5120/monash/fit5120_backend/Fit5120BackendApplication.java
@@ -0,0 +1,13 @@
+package fit5120.monash.fit5120_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fit5120BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fit5120BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fit5120/monash/fit5120_backend/Fit5120BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fit5120.monash.fit5120_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fit5120BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
