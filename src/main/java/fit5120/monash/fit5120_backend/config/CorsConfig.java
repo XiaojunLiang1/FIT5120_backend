7729f987c30ebbd42b74d68db633d25206164557
Xiaojun Liang <xlia0062@student.monash.edu>
2025-04-05 21:30:40 +1100

    ADD - Cors config set up

---
+++ src/main/java/fit5120/monash/fit5120_backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package fit5120.monash.fit5120_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://tp19-onboarding.pages.dev")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
