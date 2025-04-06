5dee4dc592ecbe40b8cb9a0a749e176a97729f9e
Xiaojun Liang <xlia0062@student.monash.edu>
2025-04-06 16:37:04 +1000

    REFACTOR - update the cors config

--- src/main/java/fit5120/monash/fit5120_backend/config/CorsConfig.java
+++ src/main/java/fit5120/monash/fit5120_backend/config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://tp19-onboarding.pages.dev")
+                        .allowedOrigins("https://tp19-onboarding.pages.dev",
+                                "http://localhost:5173",
+                                "http://127.0.0.1:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
